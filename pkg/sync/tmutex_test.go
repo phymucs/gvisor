36142c4c20c9d9e8ff8771752512c1c79c49d4b2
Ian Gudger <igudger@google.com>
2020-01-14 17:45:16 -0800

    Rename DowngradableRWMutex to RWmutex.
    Also renames TMutex to Mutex.
    
    These custom mutexes aren't any worse than the standard library versions (same
    code), so having both seems redundant.
    
    PiperOrigin-RevId: 289767814

---
+++ pkg/sync/tmutex_test.go
@@ -0,0 +1,39 @@
+// Copyright 2019 The gVisor Authors.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sync
+
+import (
+	"testing"
+)
+
+func TestDoubleTryLock(t *testing.T) {
+	var m Mutex
+	if !m.TryLock() {
+		t.Fatal("failed to aquire lock")
+	}
+	if m.TryLock() {
+		t.Fatal("unexpectedly succeeded in aquiring locked mutex")
+	}
+}
+
+func TestTryLockAfterLock(t *testing.T) {
+	var m Mutex
+	m.Lock()
+	if m.TryLock() {
+		t.Fatal("unexpectedly succeeded in aquiring locked mutex")
+	}
+}
+
+func TestTryLockUnlock(t *testing.T) {
+	var m Mutex
+	if !m.TryLock() {
+		t.Fatal("failed to aquire lock")
+	}
+	m.Unlock()
+	if !m.TryLock() {
+		t.Fatal("failed to aquire lock after unlock")
+	}
+}
